158c7d5430428bccc6e8f108113ec94539f3cafd
Jefferson Pedro <jeffersonpedro300@gmail.com>
2023-07-15 15:39:16 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("Nothing");
+
+  const handleName = () => {
+    setName(prompt("Qual é o seu Nome?"));
+  };
+
+  return (
+    <>
+      <nav onClick={handleName} className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">{name}</span>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default App;
